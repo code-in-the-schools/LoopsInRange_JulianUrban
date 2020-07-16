7364f4d022bda9484fa56f46506e25b6e3e40b53
thejduman <thejduman@yahoo.com>
2020-07-16 17:04:07 +0000

    First attempt at program

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 #for loop for range 10
+for i in range(10):
+  print(i)
+print()
+
 #for loop for range (5, 30)
+for i in range(5, 30):
+  print(i)
+print()
+
 #set int k to 10
+k = 10
 #for loop for range 10
 #print k+1
+for i in range(k):
+  k+1
+  print(k)
+print()
